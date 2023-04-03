52c34fef1fc020312023c8d583c6bc63c495c53a
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-03 21:03:18 +1200

    Using options api

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,12 +1,27 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts">
+export default {
+    // Properties returned from data() become reactive state
+    // and will be exposed on `this`.
+    data() {
+        return {
+            count: 0
+        }
+    },
 
-const count = ref(0);
+    // Methods are functions that mutate state and trigger updates.
+    // They can be bound as event listeners in templates.
+    methods: {
+        increment() {
+            this.count++
+        }
+    },
 
-const increment = () => {
-    count.value++
+    // Lifecycle hooks are called at specific points in the component lifecycle.
+    // They can be used to perform side effects, such as fetching data.
+    mounted() {
+        console.log(`The initial count is ${this.count}.`)
+    }
 }
-
 </script>
 
 <template>
